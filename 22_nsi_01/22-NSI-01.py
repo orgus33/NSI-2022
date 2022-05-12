7306f1e8706efb26fe2c394944f4a39ba10e9932
Bastien <bastien.b200405@gmail.com>
2022-05-12 22:00:20 +0200

    ex 2 sujet 1

--- 22_nsi_01/22-NSI-01.py
+++ 22_nsi_01/22-NSI-01.py
@@ -1,10 +1,16 @@
-Pieces = [100,50,20,10,5,2,1]
+Pieces = [100, 50, 20, 10, 5, 2, 1]
+
+
 def rendu_glouton(arendre, solution=[], i=0):
     if arendre == 0:
-        return ...
+        return solution
+
     p = Pieces[i]
-    if p <= ... :
-        solution.append(...)
+    if p <= arendre:
+        solution.append(p)
         return rendu_glouton(arendre - p, solution, i)
-    else :
-        return rendu_glouton(arendre, solution, ...)
+    else:
+        return rendu_glouton(arendre, solution, i+1)
+
+
+print(rendu_glouton(291, [], 0))
