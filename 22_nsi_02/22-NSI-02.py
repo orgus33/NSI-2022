10fec40a628e829d5b3ae888daf71ea59cb4260f
Bastien <bastien.b200405@gmail.com>
2022-05-12 22:33:37 +0200

    Sujet 2

--- 22_nsi_02/22-NSI-02.py
+++ 22_nsi_02/22-NSI-02.py
@@ -1,9 +1,23 @@
 def pascal(n):
-    C= [[1]]
-    for k in range(1,...):
-        Ck = [...]
-        for i in range(1,k):
-            Ck.append(C[...][i-1]+C[...][...] )
-        Ck.append(...)
+
+    C = [[1]]
+
+    for k in range(1, n+1):
+
+        Ck = [1]
+
+        for i in range(1, k):
+            print("k = ", k, "i = ", i)
+            Ck.append(C[len(C)-1][i-1] + C[len(C)-1][i])
+
+        Ck.append(1)
+
         C.append(Ck)
     return C
+
+# C : tableau abec la liste des nombres du triangles
+# n >= 0 :
+# Ck : contient la k-ième ligne du tableau
+
+
+print(pascal(5))
---
+++ 22_nsi_02/Exercice_1.py
@@ -0,0 +1,11 @@
+def moyenne(liste_note):
+    calcul = 0
+    diviseur = 0
+    for note in range(len(liste_note)):
+        calcul += liste_note[note][0] * liste_note[note][1]
+        diviseur += liste_note[note][1]
+
+    return calcul / diviseur
+
+
+print(moyenne([(15, 2), (9, 1), (12, 3)]))
