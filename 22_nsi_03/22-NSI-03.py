892985944958592388666f2d525d49c85c4021fa
Bastien <bastien.b200405@gmail.com>
2022-05-13 15:12:01 +0200

    Done

--- 22_nsi_03/22-NSI-03.py
+++ 22_nsi_03/22-NSI-03.py
@@ -3,23 +3,33 @@ class Noeud:
         self.gauche = g
         self.valeur = v
         self.droit = d
-    
+
     def __str__(self):
         return str(self.valeur)
-    
+
     def est_une_feuille(self):
         '''Renvoie True si et seulement si le noeud est une feuille'''
         return self.gauche is None and self.droit is None
 
 
 def expression_infixe(e):
-    s = ...
-    if e.gauche is not None:
-        s = '(' + s + expression_infixe(...)
-    s = s + ...
-    if ... is not None:
-        s = s + ... + ...
-    if ... :
-        return s
-    
+    s = ""
 
+    if e.gauche is not None:
+        s = '(' + s + expression_infixe(e.gauche)
+
+    s = s + str(e.valeur)
+
+    if e.droit is not None:
+        s = s + expression_infixe(e.droit) + ')'
+
+    if True:
+        return s
+
+
+e = Noeud(Noeud(Noeud(None, 3, None), '*', Noeud(Noeud(None, 8, None),
+                                                 '+', Noeud(None, 7, None))), '-', Noeud(Noeud(None, 2, None), '+',
+                                                                                         Noeud(None, 1, None)))
+
+
+print(expression_infixe(e))
---
+++ 22_nsi_03/Exercice_1.py
@@ -0,0 +1,8 @@
+def delta(liste):
+    encoding = [liste[0]]
+    for index in range(len(liste)-1):
+        encoding.append(liste[index+1] - liste[index])
+    return encoding
+
+
+print(delta([42]))
