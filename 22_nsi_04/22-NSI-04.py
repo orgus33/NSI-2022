dea114e71ad9d4ce30423eb7b2cc5ebf8faaf7e5
Bastien <bastien.b200405@gmail.com>
2022-05-13 15:39:05 +0200

    Done!

--- 22_nsi_04/22-NSI-04.py
+++ 22_nsi_04/22-NSI-04.py
@@ -1,21 +1,36 @@
 def propager(M, i, j, val):
-    if M[i][j]== ...:
+    if M[i][j] == val:
         return
 
-    M[i][j]=val
+    M[i][j] = val
 
     # l'élément en haut fait partie de la composante
-    if ((i-1) >= 0 and M[i-1][j] == ...):
+    if ((i-1) >= 0 and M[i-1][j] == 1):
         propager(M, i-1, j, val)
 
     # l'élément en bas fait partie de la composante
-    if ((...) < len(M) and M[i+1][j] == 1):
-        propager(M, ..., j, val)
+    if ((i+1) < len(M) and M[i+1][j] == 1):
+        propager(M, i+1, j, val)
 
     # l'élément à gauche fait partie de la composante
-    if ((...) >= 0 and M[i][j-1] == 1):
-        propager(M, i, ..., val)
+    if ((j-1) >= 0 and M[i][j-1] == 1):
+        propager(M, i, j-1, val)
 
     # l'élément à droite fait partie de la composante
-    if ((...) < len(M) and M[i][j+1] == 1):
-        propager(M, i, ..., val)
+    if ((j+1) < len(M) and M[i][j+1] == 1):
+        propager(M, i, j+1, val)
+
+
+M = [[0, 0, 1, 0],
+     [0, 1, 0, 1],
+     [1, 1, 1, 0],
+     [0, 1, 1, 0]]
+
+propager(M, 2, 1, 3)
+
+print(M)
+
+[[0, 0, 1, 0],
+ [0, 3, 0, 1],
+ [3, 3, 3, 0],
+ [0, 3, 3, 0]]
---
+++ 22_nsi_04/Exercice_1.py
@@ -0,0 +1,10 @@
+def recherche(tab):
+    liste = []
+    for i in range(0, len(tab)-1):
+        if (1+tab[i]) == tab[i+1]:
+            liste.append((tab[i], tab[i+1]))
+
+    return liste
+
+
+print(recherche([5, 1, 2, 3, 8, -5, -4, 7]))
